f4919061b39e50d2eaea181060220de6f30b06c6
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-11-27 11:31:43 +0100

    test

---
+++ src/main/java/com/example/galaxysafetymanger/GalaxySafetyMangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.galaxysafetymanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxySafetyMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxySafetyMangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/galaxysafetymanger/GalaxySafetyMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.galaxysafetymanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxySafetyMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
