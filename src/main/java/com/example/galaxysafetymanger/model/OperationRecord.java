2328cfcd2c3156d57dbbbb791211c1d8d5f364f8
NikoP93 <nipe0001@stud.kea.dk>
2024-12-04 13:37:57 +0100

    SKiftede navn fra "isSomething" - virker åbenbart ikke med det

--- src/main/java/com/example/galaxysafetymanger/model/OperationRecord.java
+++ src/main/java/com/example/galaxysafetymanger/model/OperationRecord.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.springframework.core.SpringVersion;
 
 import java.time.LocalDate;
 
@@ -17,9 +16,9 @@ public class OperationRecord {
     private String orName;
     private String orAddress;
     private String orPhoneNumber;
-    private boolean isEscapeRouteClear;
-    private boolean isEscapeRouteClear2;
-    private boolean isEmergencyDoorsVisible;
+    private boolean escapeRouteClear;
+    private boolean escapeRouteClear2;
+    private boolean emergencyDoorsVisible;
     private boolean emergencyLightsWork;
     private boolean warningSystemWork;
     private boolean maxCapasitiesIsVisible;
@@ -30,6 +29,103 @@ public class OperationRecord {
     private String errorDescription;
     private LocalDate dateTime;
     private String signature;
+
+
+    public LocalDate getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDate dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public boolean isEmergencyDoorsVisible() {
+        return emergencyDoorsVisible;
+    }
+
+    public void setEmergencyDoorsVisible(boolean emergencyDoorsVisible) {
+        this.emergencyDoorsVisible = emergencyDoorsVisible;
+    }
+
+    public boolean isEmergencyLightsWork() {
+        return emergencyLightsWork;
+    }
+
+    public void setEmergencyLightsWork(boolean emergencyLightsWork) {
+        this.emergencyLightsWork = emergencyLightsWork;
+    }
+
+    public boolean isEmployeeInstruction() {
+        return employeeInstruction;
+    }
+
+    public void setEmployeeInstruction(boolean employeeInstruction) {
+        this.employeeInstruction = employeeInstruction;
+    }
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+
+    public void setErrorDescription(String errorDescription) {
+        this.errorDescription = errorDescription;
+    }
+
+    public boolean isEscapeRouteClear2() {
+        return escapeRouteClear2;
+    }
+
+    public void setEscapeRouteClear2(boolean escapeRouteClear2) {
+        this.escapeRouteClear2 = escapeRouteClear2;
+    }
+
+    public boolean isEscapeRouteClear() {
+        return escapeRouteClear;
+    }
+
+    public void setEscapeRouteClear(boolean escapeRouteClear) {
+        this.escapeRouteClear = escapeRouteClear;
+    }
+
+    public boolean isEvacuationPlanForEmployee() {
+        return evacuationPlanForEmployee;
+    }
+
+    public void setEvacuationPlanForEmployee(boolean evacuationPlanForEmployee) {
+        this.evacuationPlanForEmployee = evacuationPlanForEmployee;
+    }
+
+    public boolean isFireExtinguisherIsCorrect() {
+        return fireExtinguisherIsCorrect;
+    }
+
+    public void setFireExtinguisherIsCorrect(boolean fireExtinguisherIsCorrect) {
+        this.fireExtinguisherIsCorrect = fireExtinguisherIsCorrect;
+    }
+
+    public boolean isInventoryComplieswithFloor() {
+        return inventoryComplieswithFloor;
+    }
+
+    public void setInventoryComplieswithFloor(boolean inventoryComplieswithFloor) {
+        this.inventoryComplieswithFloor = inventoryComplieswithFloor;
+    }
+
+    public boolean isMaxCapasitiesIsVisible() {
+        return maxCapasitiesIsVisible;
+    }
+
+    public void setMaxCapasitiesIsVisible(boolean maxCapasitiesIsVisible) {
+        this.maxCapasitiesIsVisible = maxCapasitiesIsVisible;
+    }
+
+    public String getOrAddress() {
+        return orAddress;
+    }
+
+    public void setOrAddress(String orAddress) {
+        this.orAddress = orAddress;
+    }
 
     public int getOrId() {
         return orId;
@@ -47,14 +143,6 @@ public class OperationRecord {
         this.orName = orName;
     }
 
-    public String getOrAddress() {
-        return orAddress;
-    }
-
-    public void setOrAddress(String orAddress) {
-        this.orAddress = orAddress;
-    }
-
     public String getOrPhoneNumber() {
         return orPhoneNumber;
     }
@@ -63,36 +151,12 @@ public class OperationRecord {
         this.orPhoneNumber = orPhoneNumber;
     }
 
-    public boolean isEscapeRouteClear() {
-        return isEscapeRouteClear;
+    public String getSignature() {
+        return signature;
     }
 
-    public void setEscapeRouteClear(boolean escapeRouteClear) {
-        isEscapeRouteClear = escapeRouteClear;
-    }
-
-    public boolean isEscapeRouteClear2() {
-        return isEscapeRouteClear2;
-    }
-
-    public void setEscapeRouteClear2(boolean escapeRouteClear2) {
-        isEscapeRouteClear2 = escapeRouteClear2;
-    }
-
-    public boolean isEmergencyDoorsVisible() {
-        return isEmergencyDoorsVisible;
-    }
-
-    public void setEmergencyDoorsVisible(boolean emergencyDoorsVisible) {
-        isEmergencyDoorsVisible = emergencyDoorsVisible;
-    }
-
-    public boolean isEmergencyLightsWork() {
-        return emergencyLightsWork;
-    }
-
-    public void setEmergencyLightsWork(boolean emergencyLightsWork) {
-        this.emergencyLightsWork = emergencyLightsWork;
+    public void setSignature(String signature) {
+        this.signature = signature;
     }
 
     public boolean isWarningSystemWork() {
@@ -103,67 +167,26 @@ public class OperationRecord {
         this.warningSystemWork = warningSystemWork;
     }
 
-    public boolean isMaxCapasitiesIsVisible() {
-        return maxCapasitiesIsVisible;
-    }
-
-    public void setMaxCapasitiesIsVisible(boolean maxCapasitiesIsVisible) {
-        this.maxCapasitiesIsVisible = maxCapasitiesIsVisible;
-    }
-
-    public boolean isInventoryComplieswithFloor() {
-        return inventoryComplieswithFloor;
-    }
-
-    public void setInventoryComplieswithFloor(boolean inventoryComplieswithFloor) {
-        this.inventoryComplieswithFloor = inventoryComplieswithFloor;
-    }
-
-    public boolean isFireExtinguisherIsCorrect() {
-        return fireExtinguisherIsCorrect;
-    }
-
-    public void setFireExtinguisherIsCorrect(boolean fireExtinguisherIsCorrect) {
-        this.fireExtinguisherIsCorrect = fireExtinguisherIsCorrect;
-    }
-
-    public boolean isEmployeeInstruction() {
-        return employeeInstruction;
-    }
-
-    public void setEmployeeInstruction(boolean employeeInstruction) {
-        this.employeeInstruction = employeeInstruction;
-    }
-
-    public boolean isEvacuationPlanForEmployee() {
-        return evacuationPlanForEmployee;
-    }
-
-    public void setEvacuationPlanForEmployee(boolean evacuationPlanForEmployee) {
-        this.evacuationPlanForEmployee = evacuationPlanForEmployee;
-    }
-
-    public String getErrorDescription() {
-        return errorDescription;
-    }
-
-    public void setErrorDescription(String errorDescription) {
-        this.errorDescription = errorDescription;
-    }
-
-    public LocalDate getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDate dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getSignature() {
-        return signature;
-    }
-
-    public void setSignature(String signature) {
-        this.signature = signature;
+    @Override
+    public String toString() {
+        return "OperationRecord{" +
+                ", orId=" + orId +
+                ", orName='" + orName + '\'' +
+                ", orAddress='" + orAddress + '\'' +
+                ", orPhoneNumber='" + orPhoneNumber + '\'' +
+                ", escapeRouteClear=" + escapeRouteClear +
+                ", escapeRouteClear2=" + escapeRouteClear2 +
+                ", emergencyDoorsVisible=" + emergencyDoorsVisible +
+                ", emergencyLightsWork=" + emergencyLightsWork +
+                ", warningSystemWork=" + warningSystemWork +
+                ", maxCapasitiesIsVisible=" + maxCapasitiesIsVisible +
+                ", inventoryComplieswithFloor=" + inventoryComplieswithFloor +
+                ", fireExtinguisherIsCorrect=" + fireExtinguisherIsCorrect +
+                ", employeeInstruction=" + employeeInstruction +
+                ", evacuationPlanForEmployee=" + evacuationPlanForEmployee +
+                ", errorDescription='" + errorDescription + '\'' +
+                ", dateTime=" + dateTime +
+                ", signature='" + signature + '\'' +
+                '}';
     }
 }
