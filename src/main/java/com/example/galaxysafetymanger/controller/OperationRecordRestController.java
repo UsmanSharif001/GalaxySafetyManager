1b812a89ea88a5d91f12b7b334b64c3b3935b7e1
thea <thsk0001@stud.kea.dk>
2024-12-08 17:48:21 +0100

    selectedOR endpoint deleteOR endpoint

--- src/main/java/com/example/galaxysafetymanger/controller/OperationRecordRestController.java
+++ src/main/java/com/example/galaxysafetymanger/controller/OperationRecordRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -23,6 +24,12 @@ public class OperationRecordRestController {
         return new ResponseEntity(or, HttpStatus.OK);
     }
 
+    @GetMapping("/selectedOperationRecord/{id}")
+    public ResponseEntity<OperationRecord> getSelectedOperationRecord(@PathVariable int id) {
+        Optional<OperationRecord> opRecord = operationRecordRepository.findById(id);
+        return opRecord.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
+
 
     @PostMapping("/createOR")
     public ResponseEntity<OperationRecord> createOR(@RequestBody OperationRecord newOR) {
@@ -30,4 +37,16 @@ public class OperationRecordRestController {
         OperationRecord savedOR = operationRecordRepository.save(newOR);
         return new ResponseEntity<>(savedOR, HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/deleteOperationRecord/{id}")
+    public ResponseEntity<Void> deleteOperationRecord(@PathVariable int id) {
+        if (operationRecordRepository.existsById(id)) {
+            operationRecordRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
+
+
