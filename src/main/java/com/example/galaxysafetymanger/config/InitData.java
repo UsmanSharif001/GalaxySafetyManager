c1e07008a8c61a36b5a50515e9e55b70d76bed84
theaskou <144421532+theaskou@users.noreply.github.com>
2024-12-14 21:12:07 +0100

    Edit or feature (#40)
    * Mockdata added
    
    * update OR endpoint

--- src/main/java/com/example/galaxysafetymanger/config/InitData.java
+++ src/main/java/com/example/galaxysafetymanger/config/InitData.java
@@ -60,9 +60,102 @@ public class InitData implements CommandLineRunner {
         operationRecord1.setFireExtinguisherIsCorrect(true);
         operationRecord1.setEmployeeInstruction(true);
         operationRecord1.setEvacuationPlanForEmployee(true);
-        operationRecord1.setDateTime(LocalDate.now());
+        operationRecord1.setDateTime(LocalDate.of(2024, 12, 3));
         operationRecord1.setSignature("ZLY");
         operationRecordRepository.save(operationRecord1);
+
+        OperationRecord operationRecord2 = new OperationRecord();
+        operationRecord2.setEscapeRouteClear(true);
+        operationRecord2.setEscapeRouteClear2(true);
+        operationRecord2.setEmergencyDoorsVisible(false);
+        operationRecord2.setEmergencyLightsWork(true);
+        operationRecord2.setWarningSystemWork(true);
+        operationRecord2.setMaxCapasitiesIsVisible(true);
+        operationRecord2.setInventoryComplieswithFloor(true);
+        operationRecord2.setFireExtinguisherIsCorrect(true);
+        operationRecord2.setEmployeeInstruction(true);
+        operationRecord2.setEvacuationPlanForEmployee(true);
+        operationRecord2.setErrorDescription("Nødbelysning over branddøren lyser meget svagt");
+        operationRecord2.setDateTime(LocalDate.of(2024, 12, 4));
+        operationRecord2.setSignature("JS");
+        operationRecordRepository.save(operationRecord2);
+
+        OperationRecord operationRecord3 = new OperationRecord();
+        operationRecord3.setEscapeRouteClear(true);
+        operationRecord3.setEscapeRouteClear2(true);
+        operationRecord3.setEmergencyDoorsVisible(true);
+        operationRecord3.setEmergencyLightsWork(true);
+        operationRecord3.setWarningSystemWork(true);
+        operationRecord3.setMaxCapasitiesIsVisible(true);
+        operationRecord3.setInventoryComplieswithFloor(true);
+        operationRecord3.setFireExtinguisherIsCorrect(true);
+        operationRecord3.setEmployeeInstruction(true);
+        operationRecord3.setEvacuationPlanForEmployee(true);
+        operationRecord3.setDateTime(LocalDate.of(2024, 12, 5));
+        operationRecord3.setSignature("EB");
+        operationRecordRepository.save(operationRecord3);
+
+        OperationRecord operationRecord4 = new OperationRecord();
+        operationRecord4.setEscapeRouteClear(true);
+        operationRecord4.setEscapeRouteClear2(true);
+        operationRecord4.setEmergencyDoorsVisible(true);
+        operationRecord4.setEmergencyLightsWork(true);
+        operationRecord4.setWarningSystemWork(true);
+        operationRecord4.setMaxCapasitiesIsVisible(true);
+        operationRecord4.setInventoryComplieswithFloor(true);
+        operationRecord4.setFireExtinguisherIsCorrect(true);
+        operationRecord4.setEmployeeInstruction(true);
+        operationRecord4.setEvacuationPlanForEmployee(true);
+        operationRecord4.setErrorDescription("Branddør i kælderen lukker ikke.");
+        operationRecord4.setDateTime(LocalDate.of(2024, 12, 6));
+        operationRecord4.setSignature("KB");
+        operationRecordRepository.save(operationRecord4);
+
+        OperationRecord operationRecord5 = new OperationRecord();
+        operationRecord5.setEscapeRouteClear(true);
+        operationRecord5.setEscapeRouteClear2(true);
+        operationRecord5.setEmergencyDoorsVisible(true);
+        operationRecord5.setEmergencyLightsWork(false);
+        operationRecord5.setWarningSystemWork(true);
+        operationRecord5.setMaxCapasitiesIsVisible(true);
+        operationRecord5.setInventoryComplieswithFloor(true);
+        operationRecord5.setFireExtinguisherIsCorrect(true);
+        operationRecord5.setEmployeeInstruction(true);
+        operationRecord5.setEvacuationPlanForEmployee(true);
+        operationRecord5.setErrorDescription("Branddør i kælderen lukker ikke.");
+        operationRecord5.setDateTime(LocalDate.of(2024, 12, 7));
+        operationRecord5.setSignature("ZLY");
+        operationRecordRepository.save(operationRecord5);
+
+        OperationRecord operationRecord6 = new OperationRecord();
+        operationRecord6.setEscapeRouteClear(true);
+        operationRecord6.setEscapeRouteClear2(true);
+        operationRecord6.setEmergencyDoorsVisible(true);
+        operationRecord6.setEmergencyLightsWork(true);
+        operationRecord6.setWarningSystemWork(true);
+        operationRecord6.setMaxCapasitiesIsVisible(true);
+        operationRecord6.setInventoryComplieswithFloor(true);
+        operationRecord6.setFireExtinguisherIsCorrect(true);
+        operationRecord6.setEmployeeInstruction(true);
+        operationRecord6.setEvacuationPlanForEmployee(true);
+        operationRecord6.setDateTime(LocalDate.of(2024, 12, 8));
+        operationRecord6.setSignature("TLS");
+        operationRecordRepository.save(operationRecord6);
+
+        OperationRecord operationRecord7 = new OperationRecord();
+        operationRecord7.setEscapeRouteClear(true);
+        operationRecord7.setEscapeRouteClear2(true);
+        operationRecord7.setEmergencyDoorsVisible(true);
+        operationRecord7.setEmergencyLightsWork(true);
+        operationRecord7.setWarningSystemWork(true);
+        operationRecord7.setMaxCapasitiesIsVisible(true);
+        operationRecord7.setInventoryComplieswithFloor(true);
+        operationRecord7.setFireExtinguisherIsCorrect(true);
+        operationRecord7.setEmployeeInstruction(true);
+        operationRecord7.setEvacuationPlanForEmployee(true);
+        operationRecord7.setDateTime(LocalDate.of(2024, 12, 8));
+        operationRecord7.setSignature("KB");
+        operationRecordRepository.save(operationRecord7);
 
     }
 }
